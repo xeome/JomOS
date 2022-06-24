7cac3f0ac51ca349f5797ae510796b8c24aca68c
xeome <xeome@protonmail.com>
2022-06-24 23:13:58 +0300

    add more information, add colors, auto install theme

--- step1.sh
+++ step1.sh
@@ -1,4 +1,45 @@
 #!/bin/bash
 
-yay -S zram-generator irqbalance timeshift-bin
+## ANSI Colors (FG & BG)
+RED="$(printf '\033[31m')" GREEN="$(printf '\033[32m')" ORANGE="$(printf '\033[33m')" BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')" CYAN="$(printf '\033[36m')" WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')" GREENBG="$(printf '\033[42m')" ORANGEBG="$(printf '\033[43m')" BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')" CYANBG="$(printf '\033[46m')" WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+
+#Get username
+username=$(whoami)
+
+## Reset terminal colors
+reset_color() {
+    printf '\033[37m'
+}
+
+echo $RED
+cat <<EOF
+              ........              │|      JomOS alpha 0.1                                                     
+         ..................         │|  JomOS is a meta Linux distribution which allows users to mix-and-match
+      ........................      │|  well tested configurations and optimizations with little to no effort 
+     ..............;ooc........     │|   
+   ................;ddl..........   │|  JomOS integrates these configurations into one largely cohesive system.
+  .................;ddl...........  │|  
+  .................;ddl...........  │|  
+  .................;ddl...........  │|  
+  .................;ddl...........  │|  
+  .................;ddl...........  │|  
+  ........:oo:.....cddc...........  │|  
+   .......'lddl::coddl'..........   │|  
+     .......,:clllc:,..........     │|  
+       .......................      │|  
+         ..................         │|  Continuing will:
+              ........              │|  - Convert existing installation into JomOS
+EOF
+
+reset_color
+yay -S zram-generator irqbalance timeshift-bin zsh vim
 sudo mv ./etc/* /etc/
+
+tar --use-compress-program=unzstd -xvf themes.tar.zst
+mkdir ~/.themes
+mv ./themes/* ~/.themes
+xfconf-query -c xsettings -p /Net/ThemeName -s "Fluent-dark"
+xfconf-query -c xfwm4 -p /general/theme -s "Fluent-dark"
