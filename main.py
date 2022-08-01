2d89d9ecb25146653da0cfc715cafffb2669d79c
xeome <xeome@proton.me>
2022-08-01 19:29:17 +0300

    more organization

--- main.py
+++ main.py
@@ -1,55 +1,27 @@
 from dataclasses import replace
-import os
 import re
 import sys
-import os.path
-from os import path
+import os
+import utils
 
-HEADER = "\033[95m"
-OKBLUE = "\033[94m"
-OKCYAN = "\033[96m"
-OKGREEN = "\033[92m"
-WARNING = "\033[93m"
-FAIL = "\033[91m"
-ENDC = "\033[0m"
-BOLD = "\033[1m"
-UNDERLINE = "\033[4m"
+USERNAME = (
+    "liveuser" if os.path.exists(
+        "/home/liveuser") else utils.exec("whoami").replace("\n", "")
+)
+
+HOMEDIR = "/home/" + USERNAME
+PHYSMEMRAW = utils.exec("grep MemTotal /proc/meminfo")
+
+PHYSMEMGB = int(re.sub("[^0-9]", "", PHYSMEMRAW)) // 1048576
+# get ram amount in kb and convert to gb with floor division
+
+SWAPPINESS = min((200 // PHYSMEMGB) * 2, 150)
+VFSCACHEPRESSURE = max(min(SWAPPINESS, 125), 32)
 
 DRYRUN = 0
 
-
-def exec(str):
-    return os.popen(str).read()
-
-
-def installdir(input, target, flags):
-    exec(
-        "find "
-        + input
-        + " -type f -exec sudo install "
-        + flags
-        + ' "{}" "'
-        + target
-        + '{}" \;'
-    )
-
-
-def replaceinfile(filepath, str, sub):
-    # Read in the file
-    with open(filepath, "r") as file:
-        filedata = file.read()
-
-    # Replace the target string
-    filedata = filedata.replace(str, sub)
-
-    # Write the file out again
-    with open(filepath, "w") as file:
-        file.write(filedata)
-
-
 COMMANDLIST = [
     "yay -S --noconfirm zram-generator irqbalance timeshift-bin zsh vim",
-    # "sudo install -o root -g root -m 644 ./etc/* /etc/",
     "tar --use-compress-program=unzstd -xvf ./assets/themes.tar.zst",
     "mkdir ~/.themes",
     "cp -r ./themes/* ~/.themes",
@@ -60,7 +32,7 @@ COMMANDLIST = [
     "bash ./chpanelcolor.sh 0 0 0 255",
 ]
 
-text = """
+ABOUT = """
               ........              │|      JomOS alpha 0.1                                                     
          ..................         │|  JomOS is a meta Linux distribution which allows users to mix-and-match
       ........................      │|  well tested configurations and optimizations with little to no effort 
@@ -79,7 +51,7 @@ text = """
               ........              │|  - Convert existing installation into JomOS
 """
 
-print(text)
+print(ABOUT)
 
 confirmation = input(
     'Please type "Confirm" without quotes at the prompt to continue: \n'
@@ -88,57 +60,46 @@ confirmation = input(
 if confirmation != "Confirm":
     print("Warning not copied exactly.")
     sys.exit()
-USERNAME = (
-    "liveuser" if path.exists(
-        "/home/liveuser") else exec("whoami").replace("\n", "")
-)
 
-# USERNAME = "liveuser"  # exec("whoami").replace("\n", "")
-HOMEDIR = "/home/" + USERNAME
-PHYSMEMRAW = exec("grep MemTotal /proc/meminfo")
 
-PHYSMEMGB = int(re.sub("[^0-9]", "", PHYSMEMRAW)) // 1048576
-# get ram amount in kb and convert to gb with floor division
-
-SWAPPINESS = min((200 // PHYSMEMGB) * 2, 150)
-VFSCACHEPRESSURE = max(min(SWAPPINESS, 125), 32)
-
-whiskermenupath = exec(
+whiskermenupath = utils.exec(
     "ls " + HOMEDIR + "/.config/xfce4/panel/whiskermenu-*.rc").replace("\n", "")
 
 # Copy system makepkg.conf for necessary modifications
 exec("cp /etc/makepkg.conf ./etc/makepkg.conf")
 
-replaceinfile("./etc/sysctl.d/99-JomOS-settings.conf",
-              "vm.swappiness = 50",
-              "vm.swappiness = " + str(SWAPPINESS)
-              )
+
+# Modify configuration files
 try:
-    replaceinfile(
+    utils.replaceinfile(
         "./etc/makepkg.conf",
         "#MAKEFLAGS=\"-j2\"",
         "MAKEFLAGS=\"-j$(nproc)\""
     )
-except:
+
+    utils.replaceinfile("./etc/sysctl.d/99-JomOS-settings.conf",
+                        "vm.swappiness = 30",
+                        "vm.swappiness = " + str(SWAPPINESS)
+                        )
+
+    utils.replaceinfile(
+        "./etc/sysctl.d/99-JomOS-settings.conf",
+        "vm.vfs_cache_pressure = 50",
+        "vm.vfs_cache_pressure = " + str(VFSCACHEPRESSURE),
+    )
+except Exception:
     # TODO: proper error handling
-    print("idk how to handle errors in python yet")
+    print("Error when modifying configurations")
 
 
-replaceinfile(
-    "./etc/sysctl.d/99-JomOS-settings.conf",
-    "vm.vfs_cache_pressure = 50",
-    "vm.vfs_cache_pressure = " + str(VFSCACHEPRESSURE),
-)
-
-# TODO: for loop running all commands from a list
 if DRYRUN != 1:
 
-    installdir("./etc", "/", "-D -o root -g root -m 644")
+    utils.installdir("./etc", "/", "-D -o root -g root -m 644")
 
     for command in COMMANDLIST:
-        exec(command)
+        utils.exec(command)
 
-    replaceinfile(
+    utils.replaceinfile(
         str(whiskermenupath),
         "button-title=EndeavourOS",
         "button-title=JomOS",
---
+++ utils.py
@@ -0,0 +1,30 @@
+import os
+
+
+def exec(str):
+    """execute a terminal command"""
+    return os.popen(str).read()
+
+
+def installdir(input, target, flags):
+    """install a directory with necessary permissions"""
+    exec(
+        "find "
+        + input
+        + " -type f -exec sudo install "
+        + flags
+        + ' "{}" "'
+        + target
+        + '{}" \;'
+    )
+
+
+def replaceinfile(filepath, str, sub):
+    """replace string in file"""
+    with open(filepath, "r") as file:
+        filedata = file.read()
+
+    filedata = filedata.replace(str, sub)
+
+    with open(filepath, "w") as file:
+        file.write(filedata)
