3bf3f6208569c239af0ab1ff0879606619483e71
xeome <xeome@proton.me>
2023-02-22 01:57:26 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,93 +1,39 @@
-import logging
-import os
 import re
 import sys
 
-from rich import print
-from rich.logging import RichHandler
 from rich.panel import Panel
+from rich import print
 
 import utils
 
-FORMAT = "%(message)s"
-logging.basicConfig(
-    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)
 
-log = logging.getLogger("rich")
+configuration = utils.parse_cli_arguments()
 
-configuration = {}
-configuration["DRY_RUN"] = 1
-configuration["THIRD_PARTY_REPOS"] = 1
-configuration["THEMING"] = 1
-
-utils.parse_arguments(
-    configuration,
-    {
-        "disable_repos": [
-            "THIRD_PARTY_REPOS",
-            0,
-            "disables usage of third party repos",
-        ],
-        "disable_theming": ["THEMING", 0, "disables theming of the OS"],
-        "enable_dry_run": ["DRY_RUN", 1, "does a dry run of the program"],
-    },
-)
 
 if configuration["DRY_RUN"]:
-    log.info("DRYRUN mode is on")
-
-V3_SUPPORT = utils.term(
-    '/lib/ld-linux-x86-64.so.2 --help | grep "x86-64-v3 (supported, searched)"'
-)
+    utils.log.info("DRYRUN mode is on")
 
 GENERIC = utils.read_file_lines("scripts/generic")
 THEMING = utils.read_file_lines("scripts/theming")
-REPOS_V3 = utils.read_file_lines("scripts/repos-v3")
 REPOS = utils.read_file_lines("scripts/repos")
 
-username = (
-    "liveuser"
-    if os.path.exists("/home/liveuser")
-    else utils.term("whoami").replace("\n", "")
-)
-
-homedir = "/home/" + username
-phys_mem_raw = utils.term("grep MemTotal /proc/meminfo")
-
-# Get ram amount in kb and convert to GiB and round
-phys_mem_gb = round(int(re.sub("[^0-9]", "", phys_mem_raw)) / 1048576)
-
-swappiness = min((200 // phys_mem_gb) * 2, 150)
-vfs_cache_pressure = int(max(min(swappiness * 1.25, 125), 32))
-
-
-zram_state = utils.term("swapon -s")
-if zram_state.find("zram") != -1:
-    log.info("This system already has zram")
-elif zram_state.find("dev") != -1:
-    log.info("This system already has physical swap")
-else:
-    log.info("System has no swap")
-
-zswap_state = utils.term("cat /sys/module/zswap/parameters/enabled")
-
-if zswap_state == "N\n":
-    log.info("Zswap is disabled")
-else:
-    log.warning("Zswap is enabled, please disable Zswap if you want to use zram.")
-
+system_info = utils.get_system_info()
+zram_state = utils.get_zram_state()
+zswap_state = utils.get_zswap_state()
 
 TWEAK_LIST = [
-    f"vm.swappiness = {swappiness}",
-    f"vm.vfs_cache_pressure = {vfs_cache_pressure}",
+    "### Memory tweaks",
+    f"vm.swappiness = {system_info['swappiness']}",
+    f"vm.vfs_cache_pressure = {system_info['vfs_cache_pressure']}",
     "vm.page-cluster = 0",
     "vm.dirty_ratio = 10",
-    "vm.dirty_background_ratio = 5",
+    "vm.dirty_background_ratio = 5\n",
+    "### Network tweaks",
     "net.core.default_qdisc = cake",
     "net.ipv4.tcp_congestion_control = bbr2",
-    "net.ipv4.tcp_fastopen = 3",
-    "kernel.nmi_watchdog = 0",
+    "net.ipv4.tcp_fastopen = 3\n",
+    "### Misc tweaks",
+    "kernel.nmi_watchdog = 0\n",
 ]
 
 ABOUT = """
@@ -109,16 +55,16 @@ confirmation = input(
 )
 
 if confirmation != "Confirm":
-    log.warning("Warning not copied exactly.")
+    utils.log.warning("Warning not copied exactly.")
     sys.exit()
 
-log.info(
-    f'USERNAME: "{username}"\nRAM AMOUNT: {phys_mem_gb}\nCALCULATED SWAPPINESS: {swappiness}\nCALCULATED '
-    f"VFS_CACHE_PRESSURE: {vfs_cache_pressure} "
+utils.log.info(
+    f'USERNAME: "{system_info["username"]}"\nRAM AMOUNT: {system_info["phys_mem_gb"]}\nCALCULATED SWAPPINESS: {system_info["swappiness"]}\nCALCULATED '
+    f"VFS_CACHE_PRESSURE: {system_info['vfs_cache_pressure']}"
 )
 
 whisker_menu_path = utils.term(
-    "ls " + homedir + "/.config/xfce4/panel/whiskermenu-*.rc"
+    "ls " + system_info["homedir"] + "/.config/xfce4/panel/whiskermenu-*.rc"
 ).replace("\n", "")
 
 # Copy system configs for necessary modifications
@@ -130,64 +76,45 @@ FILE_LIST = utils.return_files("./etc/")
 
 # Modify configuration files
 try:
-    if not configuration["DRY_RUN"]:
-        utils.replace_in_file(
-            "./etc/makepkg.conf", '#MAKEFLAGS="-j2"', 'MAKEFLAGS="-j$(nproc)"'
+    # Use all cores for makepkg
+    utils.replace_in_file(
+        "./etc/makepkg.conf", '#MAKEFLAGS="-j2"', 'MAKEFLAGS="-j$(nproc)"'
+    )
+
+    # Clear and Add header to 99-JomOS-settings.conf
+    utils.write_file("./etc/sysctl.d/99-JomOS-settings.conf", "")
+    utils.write_file(
+        "./etc/sysctl.d/99-JomOS-settings.conf",
+        "# This config file contains tweaks from JomOS and CachyOS\n\n",
+    )
+
+    # Apply tweaks from TWEAK_LIST
+    for tweak in TWEAK_LIST:
+        utils.append_to_file("./etc/sysctl.d/99-JomOS-settings.conf", tweak + "\n")
+        utils.log.info(f"Added tweak: {tweak}")
+
+    # Edit mkinitcpio.conf to use zstd compression and compression level 2
+    mkinitcpio = utils.read_file("./etc/mkinitcpio.conf")
+    if (
+        mkinitcpio.find("COMPRESSION") == 0
+        and mkinitcpio.find("#COMPRESSION_OPTIONS") == 0
+    ):
+        mkinitcpio = re.sub(
+            'COMPRESSION="(.*?)"', 'COMPRESSION="zstd"', str(mkinitcpio)
         )
+        mkinitcpio.replace("#COMPRESSION_OPTIONS=()", "COMPRESSION_OPTIONS=(-2)")
 
-        utils.replace_in_file(
-            "./etc/sysctl.d/99-JomOS-settings.conf",
-            "vm.swappiness = 50",
-            "vm.swappiness = " + str(swappiness),
-        )
-
-        utils.replace_in_file(
-            "./etc/sysctl.d/99-JomOS-settings.conf",
-            "vm.vfs_cache_pressure = 50",
-            "vm.vfs_cache_pressure = " + str(vfs_cache_pressure),
-        )
-
-        mkinitcpio = utils.read_file("./etc/mkinitcpio.conf")
-        if (
-            mkinitcpio.find("COMPRESSION") == 0
-            and mkinitcpio.find("#COMPRESSION_OPTIONS") == 0
-        ):
-            mkinitcpio = re.sub(
-                'COMPRESSION="(.*?)"', 'COMPRESSION="zstd"', str(mkinitcpio)
-            )
-            mkinitcpio.replace("#COMPRESSION_OPTIONS=()", "COMPRESSION_OPTIONS=(-2)")
-
-        utils.write_file("./etc/mkinitcpio.conf", mkinitcpio)
-
-        if V3_SUPPORT and configuration["THIRD_PARTY_REPOS"]:
-            utils.replace_in_file(
-                "./etc/pacman.conf",
-                "[core]\nInclude = /etc/pacman.d/mirrorlist",
-                "[cachyos-v3]\nInclude = /etc/pacman.d/cachyos-v3-mirrorlist\n[cachyos]\nInclude = /etc/pacman.d/cachyos-mirrorlist\n\n[core]\nInclude = /etc/pacman.d/mirrorlist",
-            )
-            utils.replace_in_file(
-                "./etc/pacman.conf",
-                "Architecture = auto",
-                "Architecture = x86_64 x86_64_v3",
-            )
-        elif configuration["THIRD_PARTY_REPOS"]:
-            utils.replace_in_file(
-                "./etc/pacman.conf",
-                "[core]\nInclude = /etc/pacman.d/mirrorlist",
-                "[cachyos]\nInclude = /etc/pacman.d/cachyos-mirrorlist\n\n[core]\nInclude = /etc/pacman.d/mirrorlist",
-            )
+    utils.write_file("./etc/mkinitcpio.conf", mkinitcpio)
 
 except Exception:
     # TODO: proper error handling
-    log.error("Error when modifying configurations")
+    utils.log.error("Error when modifying configurations")
 else:
-    log.info("File ./etc/sysctl.d/99-JomOS-settings.conf modified")
-    log.info("File ./etc/makepkg.conf modified")
-    log.info("File ./etc/mkinitcpio.conf modified")
-    log.info("File ./etc/pacman.conf modified")
+    utils.log.info("File ./etc/sysctl.d/99-JomOS-settings.conf modified")
+    utils.log.info("File ./etc/makepkg.conf modified")
+    utils.log.info("File ./etc/mkinitcpio.conf modified")
+    utils.log.info("File ./etc/pacman.conf modified")
 
-if V3_SUPPORT:
-    log.info("86-64-v3 (supported, searched)")
 
 if not configuration["DRY_RUN"]:
 
@@ -195,29 +122,25 @@ if not configuration["DRY_RUN"]:
         file_contents = utils.read_file(file)
         # Check file length, don't show it if it's longer than 2000 characters
         if len(file_contents) < 2000:
-            log.info(f"Installed file: {file}\n{file_contents}")
+            utils.log.info(f"Installed file: {file}\n{file_contents}")
         else:
-            log.info("Installed file: " + file + "\n(File too long to display)")
+            utils.log.info("Installed file: " + file + "\n(File too long to display)")
 
     # Generic commands that aren't specific to anything
     for command in GENERIC:
-        log.info("Executing command: " + command)
+        utils.log.info("Executing command: " + command)
         utils.term(command)
 
     # Adding third party repositories
-    if V3_SUPPORT and configuration["THIRD_PARTY_REPOS"]:
-        for command in REPOS_V3:
-            log.info("Executing command: " + command)
-            utils.term(command)
-    elif configuration["THIRD_PARTY_REPOS"]:
+    if configuration["THIRD_PARTY_REPOS"]:
         for command in REPOS:
-            log.info("Executing command: " + command)
+            utils.log.info("Executing command: " + command)
             utils.term(command)
 
     # Theming
     if configuration["THEMING"]:
         for command in THEMING:
-            log.info("Executing command: " + command)
+            utils.log.info("Executing command: " + command)
             utils.term(command)
         if whisker_menu_path:
             utils.replace_in_file(
@@ -227,8 +150,8 @@ if not configuration["DRY_RUN"]:
             )
 
     for tweak in TWEAK_LIST:
-        log.info(tweak)
+        utils.log.info(tweak)
 
     utils.install_dir("./etc", "/", "-D -o root -g root -m 644")
 
-    log.info("Please run sudo pacman -Syuu and reboot to complete installation.")
+    utils.log.info("Please run sudo pacman -Syuu and reboot to complete installation.")
--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
 import os
+import re
 import sys
 import rich
+import logging
+import getpass
+
+from rich.logging import RichHandler
+
+
+def setup_logging():
+    FORMAT = "%(message)s"
+    logging.basicConfig(
+        level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    )
+
+    return logging.getLogger("rich")
+
+log = setup_logging()
 
 
 def parse_arguments(config, details):
@@ -63,7 +79,7 @@ def write_file(file_path, file_data):
 
 def return_files(file_path):
     lst = list()
-    for path, sub_dirs, files in os.walk(file_path):
+    for path, _, files in os.walk(file_path):
         for name in files:
             lst.append(str(os.path.join(path, name)))
     return lst
@@ -78,3 +94,71 @@ def replace_in_file(file_path, str, sub):
 
     with open(file_path, "w") as file:
         file.write(file_data)
+
+
+def append_to_file(file_path, str):
+    """Append string to file"""
+    with open(file_path, "a") as file:
+        file.write(str)
+
+
+
+def parse_cli_arguments():
+    configuration = {
+        "DRY_RUN": 1,
+        "THIRD_PARTY_REPOS": 1,
+        "THEMING": 1,
+    }
+
+    cli_args = {
+        "disable_repos": [
+            "THIRD_PARTY_REPOS",
+            0,
+            "disables usage of third party repos",
+        ],
+        "disable_theming": ["THEMING", 0, "disables theming of the OS"],
+        "enable_dry_run": ["DRY_RUN", 1, "does a dry run of the program"],
+    }
+
+    parse_arguments(configuration, cli_args)
+
+    return configuration
+
+
+def get_system_info():
+    username = "liveuser" if os.path.exists("/home/liveuser") else getpass.getuser()
+    homedir = "/home/" + username
+
+    system_info = {
+        "username": username,
+        "homedir": homedir,
+        "phys_mem_raw": term("grep MemTotal /proc/meminfo"),
+    }
+    system_info["phys_mem_gb"] = round(
+        int(re.sub("[^0-9]", "", system_info["phys_mem_raw"])) / 1048576
+    )
+    system_info["swappiness"] = min((200 // system_info["phys_mem_gb"]) * 2, 150)
+    system_info["vfs_cache_pressure"] = int(
+        max(min(system_info["swappiness"] * 1.25, 125), 32)
+    )
+
+    return system_info
+
+
+def get_zram_state():
+    zram_state = term("swapon -s")
+    if zram_state.find("zram") != -1:
+        log.info("This system already has zram")
+    elif zram_state.find("dev") != -1:
+        log.info("This system already has physical swap")
+    else:
+        log.info("System has no swap")
+    return zram_state
+
+def get_zswap_state():
+    zswap_state = term("cat /sys/module/zswap/parameters/enabled")
+    if zswap_state == "N\n":
+        log.info("Zswap is disabled")
+    else:
+        log.warning("Zswap is enabled, please disable Zswap if you want to use zram.")
+    return zswap_state
